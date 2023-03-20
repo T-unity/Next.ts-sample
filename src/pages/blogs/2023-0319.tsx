4d6106445c2d605c268857745681725c7f0db34f
T-unity <takadanaritoshi@gmail.com>
2023-03-20 14:25:32 +0900

    Temporary correspondence double render

--- src/pages/blogs/2023-0319.tsx
+++ src/pages/blogs/2023-0319.tsx
@@ -1,15 +1,17 @@
 import type { FC } from 'react';
 import Link from 'next/link';
-import Layout from '@/components/layout';
+// import Layout from '@/components/layout';
 
 const CurrentPost: FC = () => {
 	return (
-    <Layout>
-      <h1>Test Post</h1>
-      <p>
-				<Link href="/">ホームに戻る</Link>
-			</p>
-    </Layout>
+    <>
+      {/* <Layout> */}
+        <h1>Test Post</h1>
+        <p>
+          <Link href="/">ホームに戻る</Link>
+        </p>
+      {/* </Layout> */}
+    </>
   )
 
 };
