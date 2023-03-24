fa905c2f7455712e8e2c41b69357786136498be7
T-unity <takadanaritoshi@gmail.com>
2023-03-24 16:54:46 +0900

    WIP: Add Animation

---
+++ lib/ImageRotator.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import styles from '@/styles/Gift.module.css'
+import Image from 'next/image'
+
+type Props = {
+  initialImage: string;
+  resultImages: { hit: string; miss: string };
+  width: number;
+  height: number;
+};
+
+const ImageRotator: React.FC<Props> = ({ initialImage, resultImages, width, height }) => {
+  const [imageSrc, setImageSrc] = useState(initialImage);
+  const [animationClass, setAnimationClass] = useState('');
+
+  const handleClick = () => {
+    setAnimationClass(styles.rotateYAnimation);
+
+    setTimeout(() => {
+      const isHit = Math.random() >= 0.5;
+      setImageSrc(isHit ? resultImages.hit : resultImages.miss);
+      setAnimationClass('');
+    }, 1000);
+  };
+
+  return (
+    <Image
+      src={imageSrc}
+      alt="Image Rotator"
+      onClick={handleClick}
+      className={animationClass}
+      width={width}
+      height={height}
+    />
+  );
+};
+
+export default ImageRotator;
