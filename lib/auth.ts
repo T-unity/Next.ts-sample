93cf6a4a80c68260bdb9a084ca405a44a490cff0
T-unity <takadanaritoshi@gmail.com>
2023-04-01 14:49:37 +0900

    Change Signin Anonymously

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,16 @@
 import {
-  GoogleAuthProvider,
-  signInWithPopup,
+  // GoogleAuthProvider,
+  // signInWithPopup,
+  signInAnonymously,
   UserCredential,
   signOut,
 } from 'firebase/auth';
 import { auth } from '../firebase/client';
 
 export const login = (): Promise<UserCredential> => {
-  const provider = new GoogleAuthProvider();
-  return signInWithPopup(auth, provider);
+  // const provider = new GoogleAuthProvider();
+  // return signInWithPopup(auth, provider);
+  return signInAnonymously(auth);
 };
 
 export const logout = (): Promise<void> => {
